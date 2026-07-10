299efab8913e77dac667d2686251d435cc476724
Nova <nova@layeredmedia.com>
2026-07-10 10:57:06 -0400

    security: remove untracked scripts with hardcoded secrets
    Removed temp scripts that contained UploadPost JWT keys:
    - debug_wp.py, patch_ua.py, post_social.py, pub_article.py
    - Also removed audit temp files

--- debug_wp.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, r'C:\Users\compj\.openclaw\workspace\scripts\publishing')
-from wp_rest_api import list_posts
-
-posts = list_posts('eveonion.com', per_page=2)
-print(f'Found {len(posts)} posts')
-for p in posts:
-    print(str(p['id']) + ': ' + p['title']['rendered'])
